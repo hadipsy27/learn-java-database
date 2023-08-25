263c96239908e4bccb2ef75e85aa1d64045ba4ae
hadipsy27 <hadipsy27@gmail.com>
2023-08-25 17:05:53 +0700

    Java Database : Prepared Statement - memasukkan value ke dalam query menggunakan parameter

---
+++ src/test/java/learn/labs/database/PreparedStatementTest.java
@@ -0,0 +1,37 @@
+package learn.labs.database;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PreparedStatementTest {
+    @Test
+    void testPreparedStatement() throws SQLException {
+        Connection connection = ConnectionUtil.getDataSource().getConnection();
+
+//        String username = "admin'; #";
+//        String password = "salah";
+
+        String username = "admin";
+        String password = "admin";
+
+        String sql = "SELECT * FROM admin WHERE username = ? AND password = ?";
+
+        PreparedStatement preparedStatement = connection.prepareStatement(sql);
+        preparedStatement.setString(1, username);
+        preparedStatement.setString(2, password);
+
+        ResultSet resultSet = preparedStatement.executeQuery();
+        if (resultSet.next()) {
+            System.out.println("Sukses Login : " + resultSet.getString("username"));
+        } else {
+            System.out.println("Gagal Login");
+        }
+
+        preparedStatement.close();
+        connection.close();
+    }
+}
